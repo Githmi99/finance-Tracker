639c08e9af3baa69f378f5eed562bc622f2a9e47
githmi99 <amarathuga@gmail.com>
2024-06-19 20:33:38 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useMemo } from 'react';
-import styled from "styled-components";
+import styled from 'styled-components';
 import bg from './img/bg.png';
 import { MainLayout } from './styles/Layouts';
 import Orb from './Components/Orb/Orb';
@@ -8,66 +8,49 @@ import Dashboard from './Components/Dashboard/Dashboard';
 import Income from './Components/Income/Income';
 import Expenses from './Components/Expenses/Expenses';
 import { useGlobalContext } from './context/globalContext';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
 import Login from './Components/Login';
 import Register from './Components/Register';
+import DashboardPage from './pages/DashboardPage';
 
 function App() {
-  const [active, setActive] = useState(1);
-  const global = useGlobalContext();
-  console.log(global);
-
-  const displayData = () => {
-    switch (active) {
-      case 1:
-        return <Dashboard />;
-      case 2:
-        return <Dashboard />;
-      case 3:
-        return <Income />;
-      case 4:
-        return <Expenses />;
-      default:
-        return <Dashboard />;
-    }
-  };
-
-  const orbMemo = useMemo(() => {
-    return <Orb />;
-  }, []);
-
   return (
     <Router>
-      <AppStyled bg={bg} className="App">
-        {orbMemo}
-        <MainLayout>
-          <Navigation active={active} setActive={setActive} />
-          <main>
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/dashboard" element={displayData()} />
-              <Route path="*" element={<Navigate to="/login" />} />
-            </Routes>
-          </main>
-        </MainLayout>
-      </AppStyled>
+      {/* <AppStyled bg={bg} className='App'> */}
+      {/* {orbMemo} */}
+      {/* <MainLayout> */}
+      {/* <Navigation active={active} setActive={setActive} /> */}
+      <main>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/dashboard' element={<DashboardPage />} />
+          <Route path='*' element={<Navigate to='/login' />} />
+        </Routes>
+      </main>
+      {/* </MainLayout> */}
+      {/* </AppStyled> */}
     </Router>
   );
 }
 
 const AppStyled = styled.div`
   height: 100vh;
-  background-image: url(${props => props.bg});
+  background-image: url(${(props) => props.bg});
   position: relative;
-  main{
+  main {
     flex: 1;
     background: rgba(252, 246, 249, 0.78);
-    border: 3px solid #FFFFFF;
+    border: 3px solid #ffffff;
     backdrop-filter: blur(4.5px);
     border-radius: 32px;
     overflow-x: hidden;
-    &::-webkit-scrollbar{
+    &::-webkit-scrollbar {
       width: 0;
     }
   }
