eb9217952fda553c6828b803fd4dd015325c037f
githmi99 <amarathuga@gmail.com>
2024-06-21 16:47:12 +0530

    UPDATE

---
+++ frontend/src/Components/Sidebar.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import SignOutButton from './SignOutButton'; // adjust the path accordingly
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            {/* other sidebar items */}
+            <SignOutButton />
+        </div>
+    );
+};
+
+export default Sidebar;
